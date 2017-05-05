07a7826d691b2575fd0ed69089044f61eabb09c4
Admin Localnet <localnet@users.noreply.github.com>
2017-05-05 20:36:08 +0200

    Enable extension on VSCode

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -5,6 +5,7 @@ set -ex
 # Environment variables.
 export PACKAGER="https://travis-ci.org/${1}/builds/${2}"
 export AURDEST="$(pwd)/src"
+export VSCODE_NONFREE=1
 
 # Variables declaration.
 declare -a pkglist=()
