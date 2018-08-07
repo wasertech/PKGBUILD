e9b3ae4453d20e48ed2050d974bd05f2bd8e7bb7
Devin J. Pohly <djpohly@gmail.com>
2018-08-07 15:32:13 -0500

    pass $TRAVIS_BRANCH into pkgbuild.sh

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -8,15 +8,16 @@ export AURDEST="$(pwd)/aur"
 
 # Variables declaration.
 declare -r pkgrepo="${1#*/}"
+declare -r pkg="$3"
 
 # Remove packages from repository.
 cd "repo"
 while read pkgpackage; do
   repo-remove "${pkgrepo}.db.tar.gz" $pkgpackage
-done < <(comm -23 <(pacman -Sl $pkgrepo | cut -d" " -f2 | sort) <(aurchain "$TRAVIS_BRANCH" | sort))
+done < <(comm -23 <(pacman -Sl $pkgrepo | cut -d" " -f2 | sort) <(aurchain "$pkg" | sort))
 cd ".."
 
 # Build outdated packages.
-aursync --repo $pkgrepo --root "repo" -nr "$TRAVIS_BRANCH"
+aursync --repo $pkgrepo --root "repo" -nr "$pkg"
 
 { set +ex; } 2>/dev/null
