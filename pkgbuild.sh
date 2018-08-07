967148656b225126b4bd7871d2a1239d9e8a9acd
Devin J. Pohly <djpohly@gmail.com>
2018-08-07 16:14:12 -0500

    building single packages, no need to remove

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -10,13 +10,6 @@ export AURDEST="$(pwd)/aur"
 declare -r pkgrepo="${1#*/}"
 declare -r pkg="$3"
 
-# Remove packages from repository.
-cd "repo"
-while read pkgpackage; do
-  repo-remove "${pkgrepo}.db.tar.gz" $pkgpackage
-done < <(comm -23 <(pacman -Sl $pkgrepo | cut -d" " -f2 | sort) <(aurchain "$pkg" | sort))
-cd ".."
-
 # Build outdated packages.
 aursync --repo $pkgrepo --root "repo" -nr "$pkg"
 
