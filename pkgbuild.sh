4ae218f8c864f7b5abf7ae941eec73a2ad8cd81e
Devin J. Pohly <djpohly@gmail.com>
2018-08-09 21:11:56 -0500

    move Git manipulation outside container

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -40,26 +40,4 @@ PKGDEST=repo LC_MESSAGES=C makepkg -Lcs --noconfirm "${MAKEPKG_OPT[@]}"
 # Build repo update
 LANG=C repo-add -s "${REPO_ADD_OPT[@]}" master/pkgbuild.db.tar repo/*.pkg.*
 
-# Get author/committer info from triggering branch commit
-export GIT_AUTHOR_NAME=$(git show -s --pretty='%an') \
-    GIT_AUTHOR_EMAIL=$(git show -s --pretty='%ae') \
-    GIT_COMMITTER_NAME=$(git show -s --pretty='%cn') \
-    GIT_COMMITTER_EMAIL=$(git show -s --pretty='%ce')
-
-# Commit files from repo tarball
-mkdir -p master/files
-pushd master/files >/dev/null
-
-until
-    bsdtar -xf ../pkgbuild.files.tar -C .
-    git add .
-    git commit -m "update package $TRAVIS_BRANCH"
-    git push origin master
-do
-  git reset --hard origin/master
-  git pull --ff-only origin master
-done
-
-popd >/dev/null
-
 { set +ex; } 2>/dev/null
