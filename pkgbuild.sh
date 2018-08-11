a2b818044910141e0d10fcf28956875ed9f2ea23
Devin J. Pohly <djpohly@gmail.com>
2018-08-11 10:42:35 -0500

    if key is present, populate keyring and sign package

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -9,7 +9,19 @@ export PACKAGER="https://travis-ci.org/${1}/builds/${3}"
 declare -r pkgslug="$1"
 declare -r pkgtag="$2"
 declare -r pkgrepo="${1#*/}"
-declare -a MAKEPKG_OPT=() REPO_ADD_OPT=()
+declare SIGN_PKG=
+
+# Set up gpg options
+mkdir -p "$HOME/.gnupg"
+echo 'auto-key-retrieve:0:1' | gpgconf --change-options gpg
+#echo 'keyserver:0:"hkps%3a//pgp.mit.edu"' | gpgconf --change-options dirmngr
+if [[ -r master/signing.key ]]; then
+  gpg --import master/signing.key
+  gpg --export --armor | sudo tee /usr/share/pacman/pkgbuild.gpg >/dev/null
+  gpg -k --with-colons | grep -m1 '^fpr' | cut -d: -f10 | sed 's/$/:4:/' | sudo tee /usr/share/pacman/pkgbuild-trusted >/dev/null
+  sudo pacman-key --populate pkgbuild
+  SIGN_PKG=--sign
+fi
 
 # Enable multilib repository.
 sudo sed -i -e "/\[multilib\]/,/Include/s/^#//" "/etc/pacman.conf"
@@ -24,17 +36,8 @@ EOF
 # Sync repositories
 sudo pacman -Sy
 
-# Set up gpg options
-mkdir -p "$HOME/.gnupg"
-echo 'auto-key-retrieve:0:1' | gpgconf --change-options gpg
-#echo 'keyserver:0:"hkps%3a//pgp.mit.edu"' | gpgconf --change-options dirmngr
-if [[ -r master/signing.key ]]; then
-  gpg --import master/signing.key
-  MAKEPKG_OPT+=(--sign)
-fi
-
 # Build and sign packages
-PKGDEST=repo LC_MESSAGES=C makepkg -Lcs --noconfirm "${MAKEPKG_OPT[@]}"
+PKGDEST=repo LC_MESSAGES=C makepkg -Lcs --noconfirm $SIGN_PKG
 
 # Build repo update
 LANG=C repo-add -s master/pkgbuild.db.tar repo/*.pkg.*
