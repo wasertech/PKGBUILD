36dd43b73279104e1120180f7954cc9b164167f7
Devin J. Pohly <djpohly@gmail.com>
2020-08-10 18:39:08 -0500

    decrypt the file wherever the script is

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in signing.key.enc -out signing.key -d
+declare d
+d=$(readlink -f "$(dirname "$0")")
+openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in "$d/signing.key.enc" -out "$d/signing.key" -d
