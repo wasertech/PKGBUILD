2fd8420f052e2dc8b8cc1da56f2f299ea629e79f
Devin J. Pohly <djpohly@gmail.com>
2020-12-01 18:41:00 -0600

    fix env var

--- .github/actions/updrepo/action.sh
+++ .github/actions/updrepo/action.sh
@@ -7,13 +7,13 @@ mkdir -p repo files
 # adapted from repo-add
 pushd files >/dev/null
 if ( shopt -s nullglob; files=(*); (( ${#files[*]} )) ); then
-	tar -cJf ../repo/"$REPONAME".files.tar.xz *
-	tar -cJf ../repo/"$REPONAME".db.tar.xz --exclude files *
+	tar -cJf ../repo/"$REPO_NAME".files.tar.xz *
+	tar -cJf ../repo/"$REPO_NAME".db.tar.xz --exclude files *
 else
-	tar -cJf ../repo/"$REPONAME".files.tar.xz -T /dev/null
-	tar -cJf ../repo/"$REPONAME".db.tar.xz -T /dev/null
+	tar -cJf ../repo/"$REPO_NAME".files.tar.xz -T /dev/null
+	tar -cJf ../repo/"$REPO_NAME".db.tar.xz -T /dev/null
 fi
 popd >/dev/null
 
-ln -s "$REPONAME".db.tar.xz repo/"$REPONAME".db
-ln -s "$REPONAME".files.tar.xz repo/"$REPONAME".files
+ln -s "$REPO_NAME".db.tar.xz repo/"$REPO_NAME".db
+ln -s "$REPO_NAME".files.tar.xz repo/"$REPO_NAME".files
