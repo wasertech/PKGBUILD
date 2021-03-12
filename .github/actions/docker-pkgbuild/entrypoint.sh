dbd725e129d8b43171aba783cdb331b0ffefc8e3
Devin J. Pohly <djpohly@gmail.com>
2021-03-12 15:47:50 -0600

    move setup commands into entrypoint.sh

---
+++ .github/actions/docker-pkgbuild/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# Bootstrap script to be run as root after image is built
+
+# Update packages
+pacman -Syu --noconfirm --ignore linux --ignore linux-firmware --needed base-devel
+
+# Ensure wheel group exists (seriously??)
+groupadd -f -r wheel
+
+# Create an unprivileged user
+useradd -m -G wheel -s /bin/bash pkguser
+
+# Grant group wheel sudo rights without password.
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
+
+# Run pkgbuild script as unprivileged user
+chown -R pkguser: .
+sudo -u pkguser ./master/.github/actions/docker-pkgbuild/pkgbuild.sh
