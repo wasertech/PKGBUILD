247c50935f0c4d1b078ecee609d4344973689551
Devin J. Pohly <djpohly@gmail.com>
2021-03-12 19:45:15 -0600

    remove old travis scripts

--- prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-declare d
-d=$(readlink -f "$(dirname "$0")")
-openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in "$d/signing.key.enc" -out "$d/signing.key" -d
